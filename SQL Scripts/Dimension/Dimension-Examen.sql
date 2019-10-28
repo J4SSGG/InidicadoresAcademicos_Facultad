c2c366e9f24da2cb13538cd48c5ba5dc55ebbed4
J4SSGG <jasg.sotogutierrez@gmail.com>
2019-10-27 20:50:44 -0600

    Cambios SSIS que corrige error en merge y tabla de hechos actualizada

---
+++ SQL Scripts/Dimension/Dimension-Examen.sql
@@ -0,0 +1,19 @@
+USE Admisiones_DWH;
+
+DROP TABLE IF EXISTS Dimension.Examen;
+
+
+CREATE TABLE Dimension.[Examen](
+	[SK_Examen] [dbo].[UDT_SK] IDENTITY(1,1) NOT NULL,
+	[ID_Resultado] [int] NOT NULL,
+	[ID_ResultadoDetalle] [dbo].[UDT_PK] NULL,
+	[ID_Candidato] [int] NULL,
+	[ID_Carrera] [int] NULL,
+	[ID_Descuento] [int] NULL,
+	[ID_ExamenArea] [dbo].[UDT_PK] NULL,
+	[Area] [varchar](300) NOT NULL,
+	[NotaArea] [decimal](5, 2) NULL,
+	[Precio] [decimal](6, 2) NULL,
+	[DescripcionDescuento] [varchar](300) NOT NULL,
+	[PorcentajeDescuento] [decimal](6, 2) NULL,
+	[FechaPrueba] [datetime] NULL);
