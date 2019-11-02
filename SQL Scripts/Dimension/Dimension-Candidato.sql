cc4f461c592dcfe1078b1cd3005f6dc7cbd90838
J4SSGG <jasg.sotogutierrez@gmail.com>
2019-11-02 00:26:39 -0600

    Dimension Region agregada a Modelo Multidimensional y Cubo

--- SQL Scripts/Dimension/Dimension-Candidato.sql
+++ SQL Scripts/Dimension/Dimension-Candidato.sql
@@ -5,20 +5,15 @@ DROP TABLE IF EXISTS Dimension.Candidato;
 CREATE TABLE [Dimension].[Candidato](
 	[SK_Candidato] [dbo].[UDT_SK] IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[ID_Candidato] [dbo].[UDT_PK] NULL,
+	[ID_Municipio] [dbo].[UDT_PK] NULL,
 	[ID_Colegio] [dbo].[UDT_PK] NULL,
 	[ID_Diversificado] [dbo].[UDT_PK] NULL,
 	[NombreCandidato] [dbo].[UDT_VarcharCorto] NULL,
 	[ApellidoCandidato] [dbo].[UDT_VarcharCorto] NULL,
-	[Genero] [dbo].[UDT_VarcharMediano] NULL,
+	[Genero] [dbo].[UDT_VarcharCorto] NULL,
 	[FechaNacimiento] [dbo].[UDT_DateTime] NULL,
 	[NombreColegio] [dbo].[UDT_VarcharLargo] NULL,
-	[NombreDiversificado] [dbo].[UDT_VarcharLargo] NULL,
-	[ID_Pais] [dbo].[UDT_PK] NULL,
-	[ID_Departamento] [dbo].[UDT_PK] NULL,
-	[ID_Municipio] [dbo].[UDT_PK] NULL,
-	[NombrePais] [dbo].[UDT_VarcharMediano] NULL,
-	[NombreDepartamento] [dbo].[UDT_VarcharMediano] NULL,
-	[NombreMunicipio] [dbo].[UDT_VarcharMediano] NULL);
+	[NombreDiversificado] [dbo].[UDT_VarcharLargo] NULL);
 
 
 
--- SQL Scripts/Stagin/Staging-Candidato.sql
+++ SQL Scripts/Stagin/Staging-Candidato.sql
@@ -5,6 +5,7 @@ DROP TABLE IF EXISTS Staging.Candidato;
 CREATE TABLE [Staging].[Candidato](
 	[SK_Candidato] [dbo].[UDT_SK] IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[ID_Candidato] [dbo].[UDT_PK] NULL,
+	[ID_Municipio] [dbo].[UDT_PK] NULL,
 	[ID_Colegio] [dbo].[UDT_PK] NULL,
 	[ID_Diversificado] [dbo].[UDT_PK] NULL,
 	[NombreCandidato] [dbo].[UDT_VarcharCorto] NULL,
@@ -12,13 +13,7 @@ CREATE TABLE [Staging].[Candidato](
 	[Genero] [dbo].[UDT_VarcharCorto] NULL,
 	[FechaNacimiento] [dbo].[UDT_DateTime] NULL,
 	[NombreColegio] [dbo].[UDT_VarcharLargo] NULL,
-	[NombreDiversificado] [dbo].[UDT_VarcharLargo] NULL,
-	[ID_Pais] [dbo].[UDT_PK] NULL,
-	[ID_Departamento] [dbo].[UDT_PK] NULL,
-	[ID_Municipio] [dbo].[UDT_PK] NULL,
-	[NombrePais] [dbo].[UDT_VarcharMediano] NULL,
-	[NombreDepartamento] [dbo].[UDT_VarcharMediano] NULL,
-	[NombreMunicipio] [dbo].[UDT_VarcharMediano] NULL);
+	[NombreDiversificado] [dbo].[UDT_VarcharLargo] NULL);
 
 
 
